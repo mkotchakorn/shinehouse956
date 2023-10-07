81632525da312c3504fc9a25931ebddbafa7c2d3
Kotchakorn M <32461094+mkotchakorn@users.noreply.github.com>
2023-10-07 17:49:03 +0700

    back to original

--- src/assets/data/about/about.js
+++ src/assets/data/about/about.js
@@ -5,7 +5,7 @@ export const about = {
   title: 'SHINE HOUSE',
   concept: ['ARCHITECTURE', 'INTERIOR DESIGN', 'CONSTRUCTURE'],
   paragraph_1:
-    'aSHINE HOUSE ARCHITECTURE\nWe are an architectural firm designed for exterior - interior and construction by architects and decorators. We are providing One Stop Service with a team of expert architects and engineers to supervise every step.',
+    'SHINE HOUSE ARCHITECTURE\nWe are an architectural firm designed for exterior - interior and construction by architects and decorators. We are providing One Stop Service with a team of expert architects and engineers to supervise every step.',
   paragraph_2: 'Structural Analysis Engineer Submit a construction permit. Supervise the completion of the construction work.',
   paragraph_3: 'เป็นบริษัทสถาปัตยกรรมออกแบบภายนอก-ภายใน พร้อมก่อสร้าง โดยสถาปนิกและมัณฑนากร พร้อมให้บริการด้านการออกแบบ ก่อสร้าง',
   paragraph_4:
